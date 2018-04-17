33a8b6033003fe6c8ddc3b19e70f1627fb95503c
stratixx <darnokusex@wp.pl>
2018-04-17 10:11:20 +0200

    Poprawnie działające wyjście z zagnieżdżonych pętli

--- ISE_PD/WE_1/E12.m
+++ ISE_PD/WE_1/E12.m
@@ -1,14 +1,13 @@
 function [ Rszereg ] = E12( R )
 % funkcja zwracajaca wartosc z szeregu E12 najblizsza R
 
-e12 = [10 12 15 18 22 27 33 39 47 56 68 82] *0.01;
+e12 = [10 12 15 18 22 27 33 39 47 56 68 82];
 
-e12 = [ e12(end)/10 e12 e12(1)*10 ];
+e12 = [ e12(end)/10 e12 e12(1)*10 ] * 0.01;
 
-Rszereg = [];
+Rszereg = [0];
 
 for k=0:1:10
-e12 = e12*10;
     for n=1:1:13
         if (R>=e12(n))&&(R<=e12(n+1))
             if R < (e12(n)+e12(n+1))/2
@@ -19,6 +18,10 @@ e12 = e12*10;
             break;
         end
     end
+    if Rszereg!=0
+       break;
+    end
+    e12 = 10*e12;
 end
 end
 
