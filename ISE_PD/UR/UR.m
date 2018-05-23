8fc1432f04f85497f7f20277c62cd240135b8abe
Konrad W <darnokusex@wp.pl>
2018-05-23 10:26:31 +0200

    pd z UR gotowy uklad z zrodlem pradowym

--- ISE_PD/UR/UR.m
+++ ISE_PD/UR/UR.m
@@ -12,30 +12,31 @@ Udd = -15;
 Uee = Ubee-0.7;
 
 Riee_ideal = (Uee-Udd)/Ieeproj - 390;
-Riee = E12(Riee_ideal);
+Riee = E12(Riee_ideal)
 Ieei0 = (Uee-Udd)/(390+Riee);
 devIee = (Uee-Udd)./(390+[Riee*0.95, Riee, Riee*1.05]);
 
 % dobierz Rc13 i Rc24 zapewniaj¹ce Kuproj dla Ieei0
 gm = 0.5*Ieei0/0.025;
 Rc13_ideal = Kuproj*2/gm;
-Rc13 = E12(Rc13_ideal);
+Rc13 = E12(Rc13_ideal)
 Rc24 = Rc13;
 
 % zakres Ku od rozrzutu elementów
 gm = 0.5*devIee/0.025;
-Ku = [0.95*Rc13; Rc13; 1.05*Rc13]*gm/2;
+Ku = [0.95*Rc13; Rc13; 1.05*Rc13]*gm/2
 devKu = [ min(min(Ku)) Ku(2,2) max(max(Ku)) ];
 
 % napiêcia na kolektorach
-Uc01A = Ucc - Rc13*Ieei0;
+Uc01A = Ucc - Rc13*Ieei0
 Uc01B = Uc01A;
 devUc01A = Ucc - [0.95*Rc13; Rc13; 1.05*Rc13] * devIee;
-devUc01A = [ min(min(devUc01A)) devUc01A(2,2) max(max(devUc01A)) ];
+devUc01A = [ min(min(devUc01A)) devUc01A(2,2) max(max(devUc01A)) ]
 
 %zmiana napiêcia na kolektorach
-deltaUc = Ieei0*[0.95*Rc13 1.05*Rc13];
+deltaUc = Ieei0*[0.95*Rc13 Rc13 1.05*Rc13];
 
 % opornik w emiterach
 Ree_proj = (0 - 0.7 - Udd)/Ieeproj-1500;
 Ree = E12(Ree_proj)
+IeeR0 = (0 - 0.7 - Udd)/(Ree+1500)
