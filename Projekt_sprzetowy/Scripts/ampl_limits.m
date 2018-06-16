8b7be3100add9f6418748716e55e613794ab6657
Konrad W <darnokusex@wp.pl>
2018-06-16 19:20:14 +0200

    sprawko done

--- Projekt_sprzetowy/Scripts/ampl_limits.m
+++ Projekt_sprzetowy/Scripts/ampl_limits.m
@@ -1,6 +1,6 @@
 k=-1;
-x=linspace(0,4*pi,100);
-in =3.4*0.5*sin(x);
+x=linspace(0,2,100);
+in =2.55*0.5*sin(2*pi*x);
 out = in * k;
 
 max = 2.44/2;
@@ -11,12 +11,12 @@ out(out<min)=min;
 
 figure(1);
 hold on; box on; grid on;
-plot(x,in);
-plot(x,out);
-xlim([0 12]);
+plot(x/1000,in);
+plot(x/1000,out);
+%xlim([0 12]);
 xlabel('Czas [s]');
 ylabel('Wartoœæ sygna³ów [V]');
 legend('U_w_e','U_w_y','location','southeast');
 %title('Ograniczenie dynamiki wzmacniacza, obcinanie górnej po³ówki');
-title('Ograniczenie dynamiki wzmacniacza, obcinanie obydwu po³ówek');
-print('ogr_dyn_obydwa','-dpng')
+title('Ograniczenie dynamiki wzmacniacza, obcinanie górnej po³ówki');
+print('ogr_dyn_gora','-dpng')
